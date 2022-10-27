45c4cfc4b98874fd32dc60d7d63c9649c02dcf90
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-10-27 08:25:06 -0400

    Improving usage :wrench:

--- src/south-park-scraper.js
+++ src/south-park-scraper.js
@@ -31,7 +31,8 @@ async function fetch_Episode(episode, season_number) {
      
      for (const i in StreamData.package.video.item){
        if (StreamData.package.video.item[i].code === "not_found"){
-          FinalStreamURLS.push({error: "Not Found or Removed"})
+        foundData.push({season: seasons[episodes].season, episode: seasons[episodes].episode, title: seasons[episodes].title, description: seasons[episodes].details, preview_image: seasons[episodes].image}, {error: "Not Found or Removed"})
+         return foundData
        }
        if (StreamData.package.video.item[i]['rendition']){
          
@@ -59,7 +60,7 @@ async function fetch_Episode(episode, season_number) {
         foundData.push({season: seasons[episodes].season, episode: seasons[episodes].episode, title: seasons[episodes].title, description: seasons[episodes].details, preview_image: seasons[episodes].image})
       
       foundData.push({stream_urls: FinalStreamURLS})
-            console.log(foundData)
+           // /console.log(foundData)
     }
   }
   return foundData;
@@ -84,15 +85,25 @@ async function fetchSeason(season_number){
   const rsp = await fetch( "https://raw.githubusercontent.com/wargio/plugin.video.southpark_unofficial/addon-data/addon-data-en.json" ),
         data = await rsp.json(); 
   
+  let season_eps = []
+  
   for (const episode in data.seasons[season_number]){
     for (const x in data.seasons){
-      console.log( data.seasons[season_number][x].episode)
-      
-       southpark_Scraper(data.seasons[season_number][x].episode, season_number)
+     
+    //  console.log( data.seasons[season_number][x].episode)
+      //console.log(data.seasons)
+   season_eps.push(await southpark_Scraper(data.seasons[season_number][x].episode, season_number))
+      console.log(x)
+      if (parseInt(x) === data.seasons[season_number].length -1){
+       // console.log(season_eps)
+         return season_eps
+      }
+      //console.log(season_eps)
+    
     }
      
   }
-  
+   
 }
 
 
@@ -112,17 +123,31 @@ const rsp = await fetch( "https://raw.githubusercontent.com/wargio/plugin.video.
  
   let random = get_random(season)
   //
-  console.log(random.episode)
+ // console.log(random.episode)
 //
-  southpark_Scraper(random.episode, random.season)
+ let random_ep = await southpark_Scraper(random.episode, random.season)
+  
+  return random_ep
   
   
 }
 
 
 
- fetchSeason(0)
- fetchRandom()
-//southpark_Scraper('2', 0).then(function(search_results) {
- // console.log(search_results)
- // });  ////////////
+fetchSeason(1).then(function(search_results) {
+  console.log(search_results)
+ });  ////////////
+
+
+fetchRandom().then(function(search_results) {
+  console.log(search_results)
+ });  ////////////
+
+southpark_Scraper("1",1).then(function(search_results) {
+  console.log(search_results)
+ });  ///////////////////
+
+
+southpark_Scraper("1",24).then(function(search_results) {
+  console.log(search_results)
+ });  ///////////////////
