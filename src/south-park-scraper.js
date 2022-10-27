a1fba67ff0ca36728f5bef0869d6234b93c0d4fd
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-10-27 10:43:52 -0400

    Improved error handling :wrench:

--- src/south-park-scraper.js
+++ src/south-park-scraper.js
@@ -1,28 +1,62 @@
-"use strict";
+
+let module = {}
+module.exports = {
+
+};
+
+ // JSON Data to Fetch Episodes Info From (No Streams Included)
+ async function fetchData(){
+
+  try {
+    let result = await fetch( "https://raw.githubusercontent.com/wargio/plugin.video.southpark_unofficial/addon-data/addon-data-en.json" )
+   return result.json()
+  } catch( err ) {
+    return {error: err.message };
+  }
+  
+}
+
+  
+  
+
 
 // Fetch a South Park Episode
 async function fetch_Episode(episode, season_number) {
   // 
-
+if (!episode){
+  return {south_park_error: "No episode number provided"}
+}
   
-  const rsp = await fetch( "https://raw.githubusercontent.com/wargio/plugin.video.southpark_unofficial/addon-data/addon-data-en.json" ),
-        data = await rsp.json(); 
+  if (!season_number){
+  return {south_park_error: "No season number provided"}
+}
+ 
+  
+  
+   
+ let data = await fetchData()
+ if(data.error){
+     return {south_park_error: data.error}
+    
+ }
+ 
   
   let foundData = []
   let seasons = data.seasons[season_number]
   for (const episodes in seasons){
     if (episode === seasons[episodes].episode){
-     // console.log(`Found episode for season: ${seasons[episodes].season}, episode: ${episode} `)
+ 
      let episodelinks = seasons[episodes].mediagen
      
      let DecodedLinks = [] 
      
      let FinalStreamURLS = []
      for (const link in episodelinks){
-   //   console.log(atob(episodelinks[link]))
-        
+   
       let fetchlink = atob(episodelinks[link]).replace('http://', 'https://'); 
-     // console.log(fetchlink)
+   
+       
+       // Look for the stream / m3u urls..
      const StreamURLs =  await fetch(fetchlink)
        
     
@@ -30,10 +64,13 @@ async function fetch_Episode(episode, season_number) {
       let StreamData = await StreamURLs.json();
      
      for (const i in StreamData.package.video.item){
+       /// stream was removed or banned in area etc..
        if (StreamData.package.video.item[i].code === "not_found"){
-        foundData.push({season: seasons[episodes].season, episode: seasons[episodes].episode, title: seasons[episodes].title, description: seasons[episodes].details, preview_image: seasons[episodes].image}, {error: "Not Found or Removed"})
+        foundData.push({season: seasons[episodes].season, episode: seasons[episodes].episode, title: seasons[episodes].title, description: seasons[episodes].details, season_cover_image: `https://raw.githubusercontent.com/wargio/plugin.video.southpark_unofficial/master/imgs/${seasons[episodes].season}.jpg`,  preview_image: seasons[episodes].image}, {error: "Not Found or Removed"})
          return foundData
        }
+       
+       // valid stream links
        if (StreamData.package.video.item[i]['rendition']){
          
           if (StreamData.package.video.item[i]['rendition'].src){
@@ -48,16 +85,11 @@ async function fetch_Episode(episode, season_number) {
             
        } 
      }
-       
-      
-       
-      
-       
-       
+
 
      }
       
-        foundData.push({season: seasons[episodes].season, episode: seasons[episodes].episode, title: seasons[episodes].title, description: seasons[episodes].details, preview_image: seasons[episodes].image})
+        foundData.push({season: seasons[episodes].season, episode: seasons[episodes].episode, title: seasons[episodes].title, description: seasons[episodes].details,  season_cover_image: `https://raw.githubusercontent.com/wargio/plugin.video.southpark_unofficial/master/imgs/${seasons[episodes].season}.jpg`, preview_image: seasons[episodes].image})
       
       foundData.push({stream_urls: FinalStreamURLS})
            // /console.log(foundData)
@@ -68,7 +100,7 @@ async function fetch_Episode(episode, season_number) {
 
 async function southpark_Scraper(episode, season_number)
 {
-
+  
   try {
     let result = await fetch_Episode(episode, season_number);
    return result
@@ -76,29 +108,60 @@ async function southpark_Scraper(episode, season_number)
     console.error( err.message );
   }
   
+  
 }
 
-// Fetch all episodes for season 
+
+
+async function southpark_Core(arg1, arg2, arg3)
+{
+
+  if (arg1.toLowerCase() === "episode"){
+    return await southpark_Scraper(arg2, arg3)
+  }
+  
+    if (arg1.toLowerCase() === "random"){
+    return await fetchRandom()
+  }
+  
+    if (arg1.toLowerCase() === "season"){
+      return await fetchSeason(arg2)
+  }
+  
+}
+
+
+
+
+// Fetch all episodes for season //
 
 async function fetchSeason(season_number){
   
-  const rsp = await fetch( "https://raw.githubusercontent.com/wargio/plugin.video.southpark_unofficial/addon-data/addon-data-en.json" ),
-        data = await rsp.json(); 
+ 
+ let data = await fetchData()
+ if(data.error){
+     return {south_park_error: data.error}
+    
+ }
   
   let season_eps = []
   
   for (const episode in data.seasons[season_number]){
     for (const x in data.seasons){
-     
-    //  console.log( data.seasons[season_number][x].episode)
-      //console.log(data.seasons)
-   season_eps.push(await southpark_Scraper(data.seasons[season_number][x].episode, season_number))
-      console.log(x)
+      
+      let results = await southpark_Scraper(data.seasons[season_number][x].episode, season_number)
+      
+      if (results.south_park_error){
+       return {south_park_error: results.south_park_error}
+      } else{
+        season_eps.push(results)
+      }
+    
       if (parseInt(x) === data.seasons[season_number].length -1){
-       // console.log(season_eps)
+     
          return season_eps
       }
-      //console.log(season_eps)
+      
     
     }
      
@@ -110,8 +173,15 @@ async function fetchSeason(season_number){
 // Fetch random episode
 async function fetchRandom(){
   
-const rsp = await fetch( "https://raw.githubusercontent.com/wargio/plugin.video.southpark_unofficial/addon-data/addon-data-en.json" ),
-        data = await rsp.json(); 
+
+   
+ let data = await fetchData()
+ if(data.error){
+     return {south_park_error: data.error}
+    
+ }
+ 
+  
   
   
   function get_random (list) {
@@ -134,20 +204,10 @@ const rsp = await fetch( "https://raw.githubusercontent.com/wargio/plugin.video.
 
 
 
-fetchSeason(1).then(function(search_results) {
+
+ southpark_Core("random", "1", "1").then(function(search_results) {
   console.log(search_results)
- });  ////////////
+ });  //////////////////////////
 
-
-fetchRandom().then(function(search_results) {
-  console.log(search_results)
- });  ////////////
-
-southpark_Scraper("1",1).then(function(search_results) {
-  console.log(search_results)
- });  ///////////////////
-
-
-southpark_Scraper("1",24).then(function(search_results) {
-  console.log(search_results)
- });  ///////////////////
+// Export it to make it available outside
+module.exports.southpark_Core = southpark_Core;
